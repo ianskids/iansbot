72feb97ae467082b440e710911113e000bfcf2cd
Ians Shinichi Kudo <60131374+ianskids@users.noreply.github.com>
2021-09-24 05:56:31 +0700

    config

---
+++ config.js
@@ -0,0 +1,36 @@
+global.owner = ['6285789993325'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '82d4dc815ab1fd4c', //ab9942f95c09ca89, 65654c74a7169de8, da5fb2b73ae3e451, 10c4105200edc0f0
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = '᳄ ɪͥᴀͭɴᷤꜱ ʙᷟᴏͤᴛ ᳄'
+global.author = '© Ians'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
